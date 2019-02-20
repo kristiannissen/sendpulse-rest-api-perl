ea9546d92d997e43ef6f0dbfdea4c45358010318
Kristian Nissen <kristian.nissen@gmail.com>
2019-02-20 13:47:11 +0000

    Installed Test modules and added ARGV for secret and id

--- tests/smtp.pl
+++ tests/smtp.pl
@@ -4,20 +4,24 @@ use 5.10.0;
 use strict;
 use warnings;
 use diagnostics;
+use Carp qw(carp croak);
 
 use FindBin qw($Bin);
 use lib "$Bin/../";
 
-# use Test::More;
-# use Test::Output;
+use Test::More;
+use Test::Output;
 
 use Data::Dumper;
 
 use SendPulse::RestApi;
 
+# Accept id and secret
+my ($client_id, $client_secret) = @ARGV;
+
 my $api = SendPulse::RestApi->new(
-        client_secret => "kitty",
-        client_id => "Hello"
+        client_secret => $client_secret,
+        client_id => $client_id
     );
 
 say Dumper($api);
