38bf524bb09b3be9209cd0c02206e43d397ec1e2
Kristian <kn@unisport.dk>
2019-09-03 17:50:00 +0200

    Working on new solution

--- SendPulse/RestApi.pm
+++ SendPulse/RestApi.pm
@@ -30,86 +30,17 @@ sub new {
 
     return $this;
 }
-# Magic getter/setter methods
-sub client_id {
-    my ($this, $id) = @_;
-    if ($id) {
-        $this->{client_id} = $id;
-    } else {
-        return $this->{client_id};
-    }
-}
-sub client_secret {
-    my ($this, $secret) = @_;
-    if ($secret) {
-        $this->{client_secret} = $secret;
-    } else {
-        return $this->{client_secret};
-    }
-}
-# Make request to the API endpoint
-# this->make_post_request(URL, []);
-sub make_post_request {
-    my ($this, $url, @params) = @_;
+# Post request method
+# Get request method
+sub make_get_request {
+    my ($this, $header, $url, @params) = @_;
 
-    my $request = POST $url, @params;
+    my $req = GET $url, @params;
+    $req->header($header);
 
-    # Use the token if it exists
-    $request->header("Authorization" => 
-        $this->{token_type} ." ". $this->{token}
-    ) if ($this->{token});
+    my $res = $this->{ua}->request($req);
 
-    my $response = $this->{ua}->request($request);
-    my $json_response = decode_json($response->content);
-
-    $json_response->{http_status_code} = $response->code;
-    $json_response->{http_message} = $response->message;
-
-    return $json_response;
-}
-# Request Authorization token
-# If there is already a request token don't request a new one
-sub request_token {
-    my ($this) = @_;
-
-    my $json_auth = $this->make_post_request("https://api.sendpulse.com/oauth/access_token", [
-        "grant_type" => $this->{grant_type},
-        "client_id" => $this->{client_id},
-        "client_secret" => $this->{client_secret}
-    ]);
-
-    return 0 if $json_auth->{error};
-
-    $this->{token} = $json_auth->{access_token};
-    $this->{token_type} = $json_auth->{token_type};
-    $this->{expires} = $json_auth->{expires_in};
-
-    return 1;
-}
-# Send email to list of recipients
-sub send_emails {
-    my ($this, %email_data) = @_;
-
-    # HTML has to be base64 encoded
-    $email_data{"html"} = encode_base64($email_data{"html"});
-    my $json_response = $this->make_post_request("https://api.sendpulse.com/smtp/emails", [
-            "email" => encode_json(\%email_data)
-        ]);
-    
-    # Return result
-    return ($json_response->{result}, $this->total_emails_sent());
-}
-# Retrieve total amount of sent emails
-sub total_emails_sent {
-    my ($this) = @_;
-    my $request = GET "https://api.sendpulse.com/smtp/emails/total";
-    $request->header("Authorization" => 
-        $this->{token_type} ." ". $this->{token}
-    ) if ($this->{token});
-    my $response = $this->{ua}->request($request);
-    my $json_response = decode_json($response->content);
-    
-    return $json_response->{total};
+    return ($res->code, decode_json($res->content));
 }
 
 1;
--- tests/smtp.pl
+++ tests/smtp.pl
@@ -26,57 +26,14 @@ my $api = SendPulse::RestApi->new(
         client_id => $client_id
     );
 
-# Property assigning
-ok ($api->client_id eq $client_id, "Assign client id");
-
-ok ($api->client_secret eq $client_secret, "Assign client secret");
-
-# Test wrong credentials
-my $error = $api->make_post_request("https://api.sendpulse.com/oauth/access_token", []);
-
-ok ($error->{error_code} eq 1, "Test Unauthorized");
-
-# Test correct credentials
-my $success = $api->make_post_request("https://api.sendpulse.com/oauth/access_token", [
-                "client_id" => $client_id,
-                "client_secret" => $client_secret,
-                "grant_type" => "client_credentials"
-        ]);
-
-ok ($success->{http_status_code} eq "200", "Test Authorized");
-
-# Test Authorization token
-# Pass is wrong secret
-$api->client_secret("HelloKitty");
-
-ok ($api->request_token() eq 0, "Wrong Request Authorization token");
-
-# Change to corret secret
-$api->client_secret($client_secret);
-
-ok ($api->request_token() eq 1, "Correct Request Authorization token");
-
-# Test send_emails
-
-my %email_data = (
-    "html" => "Hello Kitty in html",
-    "text" => "Hello Kitty in plain text",
-    "subject" => "Hello from the Kitty",
-    "from" => {
-        "name" => "Hello Kitty",
-        "email" => $sender
-    },
-    "to" => [
-        {"email" => $receiver}
-    ]
+## TODO test make_get_request works
+my ($code, $response) = $api->make_get_request(
+    {},
+    "https://api.sendpulse.com/smtp/emails/total",
+    []
 );
+ok ($code eq 400, "Test bad request code");
+ok (exists($response->{'error'}), "Test bad request response");
 
-my ($result, $mails_sent) = $api->send_emails(%email_data);
-ok ($result eq 1, "Email was sent");
-ok ($mails_sent gt 0, "Sent more than 0 emails");
-
-# Test number of emails send
-ok($api->total_emails_sent() gt 0, "Number of emails sent is higher than 0");
-
-# Done testing
+## TODO test that the request_token works
 done_testing(plan());
